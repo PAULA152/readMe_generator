cbd2c9fc391179338b4f48f54b711991b26eb051
PAULA152 <99390832+PAULA152@users.noreply.github.com>
2022-05-01 10:09:47 +0000

    Add files via upload

---
+++ Develop/index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown, renderLicenseBadge, renderLicenseSection, renderLicenseLink } = require('./utils/generateMarkdown');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation steps.',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Provide examples for use.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, any third-party assets, or any other features.',
+        name: 'credits',
+    },
+    {
+        type: 'checkbox',
+        message: 'Select Licenses',
+        name: 'license',
+        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC License', 'GNU GPLv2',],
+    },
+];
+
+function init() {
+    inquirer
+        .prompt([...questions])
+        .then((answers) => {
+            fs.writeFile(`${answers.projectTitle}.md`, generateMarkdown(answers), (err) =>
+                err ? console.log(err) : console.log('SUCCESS!'))
+        })
+};
+
+init();
+
---
+++ Develop/project.md
@@ -0,0 +1,28 @@
+# project
+  ## Description
+
+  this is an example of a project
+
+  ## Table of Contents 
+
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [Credits](#Credits)
+  4. [License](#License)
+
+  ## Installation
+
+  no install required
+
+  ## Usage
+
+  an example
+
+  ## Credits
+
+  there are none
+
+  ## License
+  Apache License 2.0,GNU GPLv3,ISC License,GNU GPLv2
+
+  
